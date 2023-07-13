72923458b6e90d398eb76539997a50f289580f21
Andreaacag <135671655+Andreaacag@users.noreply.github.com>
2023-07-13 01:53:44 +0000

    Tercera Tarea

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const num_who = Math.floor(Math.random() * 4);
+  const num_action = Math.floor(Math.random() * 4);
+  const num_what = Math.floor(Math.random() * 3);
+  const num_when = Math.floor(Math.random() * 5);
+
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `<h1>${who[num_who]} ${action[num_action]} ${what[num_what]} ${when[num_when]}</h1>`;
 };
